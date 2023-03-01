998b37f2e822f317765b127bd626b2ef294b1d31
Tomoya Koyanagi <tomk79@gmail.com>
2023-03-01 13:59:24 +0900

    エラーを、 `$px->error()` にも報告するようになった。

--- php/register.php
+++ php/register.php
@@ -13,9 +13,6 @@ class register{
 	/** プラグインオプション */
 	private $options;
 
-	/** px2-clover */
-	private $clover;
-
 
 	/**
 	 * entry
@@ -26,40 +23,54 @@ class register{
 	static public function register( $px = null, $options = null ){
 		if( count(func_get_args()) <= 1 ){
 
-            $options = $px;
+			$options = (object) $px;
 
-            // 例外ハンドラを設定する
-            set_exception_handler(function(Throwable $exception) use ($options){
-                $datestr = date('Y-m-d H:i:s');
-                echo "Uncaught exception: ", $exception->getMessage(), "\n";
-                if( $options['realpath_log_dir'] ?? false && is_dir($options['realpath_log_dir']) ){
-                    error_log(
-                        $datestr." - Uncaught exception: ".$exception->getMessage().' on '.$exception->getFile().' line:'.$exception->getLine()."\n",
-                        3,
-                        $options['realpath_log_dir'].'/error_report.log'
-                    );
-                }
-            });
-
-            // エラーハンドラを設定する
-            set_error_handler(function($errno, $errstr, $errfile, $errline) use ($options){
-                $datestr = date('Y-m-d H:i:s');
-                if( $options['realpath_log_dir'] ?? false && is_dir($options['realpath_log_dir']) ){
-                    error_log(
-                        $datestr.' - Error['.$errno.']: '.$errstr.' on '.$errfile.' line:'.$errline."\n",
-                        3,
-                        $options['realpath_log_dir'].'/error_report.log'
-                    );
-                }
- 
-                return false;
-            });
-
+			self::register_error_handler(null, $options);
 
 			return __CLASS__.'::'.__FUNCTION__.'('.( is_array($px) ? json_encode($px) : '' ).')';
 		}
 
+		self::register_error_handler($px, $options);
+
 		return;
+	}
+
+	/**
+	 * エラーハンドラを登録する
+	 */
+	static private function register_error_handler($px, $options){
+		// 例外ハンドラを設定する
+		set_exception_handler(function(Throwable $exception) use ($px, $options){
+			$datestr = date('Y-m-d H:i:s');
+			echo "Uncaught exception: ", $exception->getMessage(), "\n";
+			if( $px ){
+				$px->error( "Uncaught exception: ", $exception->getMessage() );
+			}
+			if( $options->realpath_log_dir ?? false && is_dir($options->realpath_log_dir) ){
+				error_log(
+					$datestr." - Uncaught exception: ".$exception->getMessage().' on '.$exception->getFile().' line:'.$exception->getLine()."\n",
+					3,
+					$options->realpath_log_dir.'/error_report.log'
+				);
+			}
+		});
+
+		// エラーハンドラを設定する
+		set_error_handler(function($errno, $errstr, $errfile, $errline) use ($px, $options){
+			$datestr = date('Y-m-d H:i:s');
+			if( $px ){
+				$px->error( 'Error['.$errno.']: '.$errstr.' on '.$errfile.' line:'.$errline );
+			}
+			if( $options->realpath_log_dir ?? false && is_dir($options->realpath_log_dir) ){
+				error_log(
+					$datestr.' - Error['.$errno.']: '.$errstr.' on '.$errfile.' line:'.$errline."\n",
+					3,
+					$options->realpath_log_dir.'/error_report.log'
+				);
+			}
+
+			return false;
+		});
 	}
 
 }
